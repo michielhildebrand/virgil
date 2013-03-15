6657742be9bc8e8bf02e1a2eef66d5f3edf6045a
Michiel Hildebrand <m.hildebrand@vu.nl>
2013-03-15 14:58:53 +0100

    FIX prr computation. Use drug-reaction cooccurrences instead of report count

--- lib/aers_report.pl
+++ lib/aers_report.pl
@@ -2,11 +2,23 @@
 	  [report/4,
 	   report_by_drug/2,
 	   report_by_filter/2,
-	   reaction/4
+	   reaction/4,
+	   drug_reaction_counts/2,
+	   reaction_count/2,
+	   cc_count/1,
+	   flush_virgil_cache/0
 	  ]).
 
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(semweb/rdf_label)).
+
+:- dynamic
+	virgil_cache/2.
+
+flush_virgil_cache :-
+	retractall(virgil_cache(_, _)).
+
+%%	report(+DrugMentionList, -ReactionTerms, +ReportFilter, -Report)
 
 report(Drugs, Reactions, Filter, Report) :-
 	(   Drugs = [_|_]
@@ -33,7 +45,8 @@ report_by_reaction([_|T], Report) :-
 report_by_filter([], Report) :-
 	rdf(Report, rdf:type, aers:'Report').
 
-%%	reaction(+Drugs, +Filter, ?Reaction, -Report)
+
+%%	reaction(+DrugMentionList, +Filter, ?Reaction, -Report)
 
 reaction(Drugs, Filter, Reaction, Report) :-
 	(   Drugs = [_|_]
@@ -43,3 +56,54 @@ reaction(Drugs, Filter, Reaction, Report) :-
 	report_by_filter(Filter, Report),
 	rdf(Report, aers:reaction, Reaction_Lit),
 	literal_text(Reaction_Lit, Reaction).
+
+
+%%	drug_reacton_counts(+DrugMentionList, -Pairs:count-reaction)
+
+drug_reaction_counts(Drugs, DRs) :-
+	virgil_cache(drug(Drugs), DRs),
+	!.
+drug_reaction_counts(Drugs, DRs) :-
+	findall(Count-Reaction,
+		(setof(R, reaction(Drugs, [], Reaction, R), Rs),
+		 length(Rs, Count)
+		), DRs),
+	assert(virgil_cache(drug(Drugs), DRs)).
+
+
+%%	reaction+_count(+Reaction, -ReactionCount)
+
+reaction_count(Reaction, Count) :-
+	virgil_cache(reaction(Reaction), Count),
+	!.
+reaction_count(Reaction, Count) :-
+	setof(R-D, (report([], [Reaction], [], R),
+		    report_drug_name(R, D)
+		   ),
+	      Es),
+	length(Es, Count),
+	assert(virgil_cache(reaction(Reaction), Count)).
+
+%%	cc_count(-Count)
+
+cc_count(Count) :-
+	virgil_cache(cc, Count),
+	!.
+cc_count(Count) :-
+	setof(R-D-E, report_drug_reaction([], R, D, E), As),
+	length(As, Count),
+	assert(virgil_cache(cc, Count)).
+
+
+report_drug_reaction(Filter, Report, DrugName, Reaction) :-
+	report_by_filter(Filter, Report),
+	report_drug_name(Report, DrugName),
+	report_reaction(Report, Reaction).
+
+report_drug_name(Report, Drug) :-
+	rdf(Report, aers:drug, DrugUse),
+	rdf(DrugUse, aers:drugname, literal(Drug)).
+
+report_reaction(Report, Reaction) :-
+	rdf(Report, aers:reaction, literal(Reaction)).
+
